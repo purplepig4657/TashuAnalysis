338f1f41d1181cc235a996a160f3bb014a976be0
purplepig4657 <80941516+purplepig4657@users.noreply.github.com>
2023-06-07 20:00:33 +0900

    fead: drop row station is nan

--- src/transform/rent/rent_preprocessor.py
+++ src/transform/rent/rent_preprocessor.py
@@ -1,6 +1,8 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import pandas as pd
 
+from src.base.column_name import RentDataCN
+
 
 class RentPreprocessor(BaseEstimator, TransformerMixin):
     """
@@ -13,6 +15,11 @@ class RentPreprocessor(BaseEstimator, TransformerMixin):
     def fit(self, X: dict, y=None):
         return self
 
-    # noinspection PyMethodMayBeStatic
     def transform(self, X: pd.DataFrame, y=None) -> pd.DataFrame:
+        X = self.drop_nan_rent_station(X)
+        return X
+
+    # noinspection PyMethodMayBeStatic
+    def drop_nan_rent_station(self, X: pd.DataFrame) -> pd.DataFrame:
+        X.dropna(subset=[RentDataCN.RENT_STATION], inplace=True)
         return X
