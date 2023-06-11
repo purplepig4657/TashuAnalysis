391a3e78ff3bbd127ebc6c84a08c17e661f2405f
eunbin777 <103042314+eunbin777@users.noreply.github.com>
2023-06-11 16:24:50 +0900

    feat : modify rent_preprocessing

--- src/transform/rent/rent_preprocessor.py
+++ src/transform/rent/rent_preprocessor.py
@@ -17,9 +17,39 @@ class RentPreprocessor(BaseEstimator, TransformerMixin):
 
     def transform(self, X: pd.DataFrame, y=None) -> pd.DataFrame:
         X = self.drop_nan_rent_station(X)
+        X = self.drop_nan_return_station(X)
+        X = self.drop_zero_station(X)
+        X = self.drop_distance_column(X)
+        X = self.drop_recent_data(X)
+        X = self.drop_abnormal_data(X)
         return X
 
     # noinspection PyMethodMayBeStatic
     def drop_nan_rent_station(self, X: pd.DataFrame) -> pd.DataFrame:
         X.dropna(subset=[RentDataCN.RENT_STATION], inplace=True)
+        return X
+    
+    # noinspection PyMethodMayBeStatic
+    def drop_nan_return_station(self, X: pd.DataFrame) -> pd.DataFrame:
+        X.dropna(subset=[RentDataCN.RETURN_STATION], inplace=True)
+        return X
+    
+    # noinspection PyMethodMayBeStatic
+    def drop_zero_station(self, X: pd.DataFrame) -> pd.DataFrame:
+        X = X.drop(X[(X[RentDataCN.RENT_STATION] == 0) | (X[RentDataCN.RETURN_STATION] == 0)].index, inplace=True)
+        return X
+    
+    # noinspection PyMethodMayBeStatic
+    def drop_distance_column(self, X: pd.DataFrame) -> pd.DataFrame:
+        X = X.drop(labels = RentDataCN.DISTANCE, axis = 1, inplace=True)
+        return X
+    
+    # noinspection PyMethodMayBeStatic
+    def drop_recent_data(self, X: pd.DataFrame) -> pd.DataFrame:
+        X = X.drop(X[(X[RentDataCN.RENT_STATION] > 262) | (X[RentDataCN.RETURN_STATION] > 262)].index, inplace=True)
+        return X
+    
+    # noinspection PyMethodMayBeStatic
+    def drop_abnormal_data(self, X: pd.DataFrame) -> pd.DataFrame:
+        X.drop(X[(X[RentDataCN.RENT_STATION] == X[RentDataCN.RETURN_STATION]) & (X[RentDataCN.RETURN_DATE] - X[RentDataCN.RENT_DATE] <= pd.Timedelta(minutes=5))].index, inplace=True)
         return X
